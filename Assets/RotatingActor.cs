b7f61a55ebeb26c9f6212869e52cdb0be50479aa
Ann-Laurie <lapointeannlaurie@gmail.com>
2021-09-18 13:06:51 -0400

    Add texture and rotation

--- Assets/RotatingActor.cs
+++ Assets/RotatingActor.cs
@@ -13,6 +13,6 @@ public class RotatingActor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 15 * Time.deltaTime, 0);
     }
 }
