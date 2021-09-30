cdec1408ed4211751187c7a37721389d9cd974ae
Ann-Laurie Lapointe <lapointeannlaurie@gmail.com>
2021-09-30 16:00:56 -0400

    Load next scene after fade out

--- Assets/FadeLogo.cs
+++ Assets/FadeLogo.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FadeLogo : MonoBehaviour
 {
@@ -29,5 +30,7 @@ public class FadeLogo : MonoBehaviour
         fadeIn();
         yield return new WaitForSeconds(2);
         fadeOut();
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
