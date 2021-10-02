fbb49b6c37f226a7890d2a2c2dfcc61458736c71
Ann-Laurie Lapointe <lapointeannlaurie@gmail.com>
2021-10-02 16:02:01 -0400

    Create level and add finish line

---
+++ Assets/Scripts/OnSphereCollision.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnSphereCollision : MonoBehaviour
+{
+    public AudioSource collisionSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionSound = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            collisionSound.Play();
+        }
+
+        if (collision.gameObject.tag == "Finish")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
