fc93f663dcaa1b83c6408398cd11b541d04e6bc6
Ann-Laurie Lapointe <lapointeannlaurie@gmail.com>
2021-10-02 14:47:03 -0400

    Add countdown and ball dropping

---
+++ Assets/Scripts/Decompte.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Decompte : MonoBehaviour
+{
+    public float time = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(timer());
+        time += 1;
+    }
+
+    IEnumerator timer()
+    {
+        while(time > 0)
+        {
+            time--;
+            yield return new WaitForSeconds(1f);
+            GetComponent<Text>().text = time.ToString();
+        }
+
+        if (time == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
