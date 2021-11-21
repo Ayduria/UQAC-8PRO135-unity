d0530de6f968456e6ed3af0aa22e63ca6a019c74
Ann-Laurie <lapointeannlaurie@gmail.com>
2021-11-21 15:36:30 -0500

    Animate character

--- Assets/Game/MainCharacter/ThirdPersonMovement.cs
+++ Assets/Game/MainCharacter/ThirdPersonMovement.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
+    public Animator characterAnimator;
     public CharacterController controller;
     public Transform cam;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    private void Start()
+    {
+        characterAnimator = gameObject.GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,12 +25,17 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if(direction.magnitude >= 0.1f)
         {
+            characterAnimator.SetBool("Moving", true);
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        else
+        {
+            characterAnimator.SetBool("Moving", false);
         }
     }
 }
