78362fec146c466c4a14f87c4cbfad6075eeadc4
Ann-Laurie <lapointeannlaurie@gmail.com>
2021-11-23 14:17:08 -0500

    Add lawnmower count

--- Assets/Game/Scripts/ItemPickup.cs
+++ Assets/Game/Scripts/ItemPickup.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemPickup : MonoBehaviour
 {
+    private int totalCount = 0;
+    public Text lawnmowerCount;
+
+
+    private void Update()
+    {
+        lawnmowerCount.text = "Tondeuses: " + totalCount;
+    }
+
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.gameObject.tag == "Lawnmower")
         {
-            Debug.Log("lol");
+            totalCount++;
             Destroy(hit.gameObject);
         }
     }
