992acfc19e0061fe514d4d343cc636ecc726cb78
Ann-Laurie <lapointeannlaurie@gmail.com>
2021-11-23 15:33:12 -0500

    Add victory and defeat conditions

--- Assets/Game/Scripts/ItemPickup.cs
+++ Assets/Game/Scripts/ItemPickup.cs
@@ -2,24 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ItemPickup : MonoBehaviour
 {
-    private int totalCount = 0;
+    private int totalLawnmowerCount = 0;
+    private int totalEnemyCount;
     public Text lawnmowerCount;
+    public Text enemyCount;
+    private AudioSource pickupSound;
+    private GameObject[] cptEnnemis;
 
+    private void Start()
+    {
+        cptEnnemis = GameObject.FindGameObjectsWithTag("Enemy");
+        totalEnemyCount = cptEnnemis.Length;
+    }
 
     private void Update()
     {
-        lawnmowerCount.text = "Tondeuses: " + totalCount;
+        lawnmowerCount.text = "Tondeuses: " + totalLawnmowerCount;
+        enemyCount.text = "Ennemis restants: " + totalEnemyCount;
+        pickupSound = GetComponent<AudioSource>();
+
+        if (totalEnemyCount == 0)
+        {
+            SceneManager.LoadScene("Victory");
+        }
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.gameObject.tag == "Lawnmower")
         {
-            totalCount++;
             Destroy(hit.gameObject);
+            pickupSound.Play();
+            totalLawnmowerCount++;
+        }
+
+        if (hit.gameObject.tag == "Enemy")
+        {
+            if (totalLawnmowerCount > 0)
+            {
+                Destroy(hit.gameObject);
+                totalLawnmowerCount--;
+                totalEnemyCount--;
+            } else {
+                Debug.Log("You died lol");
+                Destroy(gameObject);
+            }
         }
     }
 }
