87b1adfeccc2f218ae2fa27cb9ddea192749ed6b
Patrick McRea <90723319+PatMcRea@users.noreply.github.com>
2021-12-16 15:38:14 -0500

    Add shooting mechanic

---
+++ Assets/Space game/Scripts/Gun.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bullet;
+
+    public float shootForce, upwardForce;
+
+    //public float timebetweenShooting, spread, timeBetweenShots;
+    //public int bulletsPerTap;
+
+    //int bulletsLeft, bulletsShot;
+
+    bool shooting, readyToShoot, reloading;
+
+    public Camera cam;
+    public Transform attackPoint;
+
+    private void Awake() 
+    {
+        readyToShoot = true; 
+    }
+
+    private void Update() 
+    {
+        MyInput();
+    }
+    private void MyInput() 
+    {
+        if (shooting = Input.GetKeyDown(KeyCode.Mouse0)) {Shoot(); };
+    }
+
+    private void Shoot() 
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+
+        if (Physics.Raycast(ray, out hit)) { targetPoint = hit.point; Debug.Log("SHOT"); }
+        else { targetPoint = ray.GetPoint(75); }
+
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+        currentBullet.transform.forward = directionWithoutSpread.normalized;
+
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithoutSpread.normalized * shootForce, ForceMode.Impulse);
+    }
+}
--- Assets/Space game/Scripts/ShipController.cs
+++ Assets/Space game/Scripts/ShipController.cs
@@ -2,65 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-/*public class ShipController : MonoBehaviour
-{
-    PlayerController playerControls;
-    Rigidbody shipRb; 
-    [SerializeField] float speed;
-    [SerializeField] float angle_x;
-    [SerializeField] float angle_y;
-    [SerializeField] float rotationSpeed;
-    float boostInput;
-    Vector2 move;
-
-    void Awake()
-    {
-        playerControls = new PlayerController();
-        playerControls.Gameplay.Boost.performed += onBoost;
-        playerControls.Gameplay.Boost.canceled += onBoost;
-        playerControls.Gameplay.Move.performed += ctx => move = (ctx.ReadValue<Vector2>());
-        playerControls.Gameplay.Move.canceled += ctx => move = Vector2.zero;
-    }
-
-    void onBoost(InputAction.CallbackContext value) 
-    {
-        boostInput = value.ReadValue<float>();
-    
-    }
-    private void OnEnable()
-    {
-        playerControls.Gameplay.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerControls.Gameplay.Disable(); 
-    }
-
-
-    void Start()
-    {
-        shipRb = GetComponent<Rigidbody>(); 
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        shipRb.velocity = transform.forward * speed * (Mathf.Max(boostInput, .2f));
-        turnShip();
-    }
-    void turnShip() 
-    {
-        transform.Rotate(Vector3.up, move.x * rotationSpeed * Time.deltaTime);
-        turnShipVisual();
-    }
-    void turnShipVisual() 
-    {
-        transform.localEulerAngles = new Vector3(move.y * angle_y, transform.localEulerAngles.y, -move.x * angle_x);
-    } 
-
-}*/
-
 
 public class ShipController : MonoBehaviour 
 {
@@ -68,7 +9,7 @@ public class ShipController : MonoBehaviour
     private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
     private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
 
-    private float lookRateSpeed = 90f;
+    public float lookRateSpeed = 90f;
     private Vector2 lookInput, screenCenter, mouseDistance;
 
     private float rollInput;
